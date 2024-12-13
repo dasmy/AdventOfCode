4b62621d607795e37603930bbef2aa14e7f187e8
Mathias Winkel <mathias.winkel@merckgroup.com>
2024-12-13 09:49:25 +0100

    13 2024

---
+++ 2024/day_13b.py
@@ -0,0 +1,43 @@
+from day_13inp import INPUT
+import re
+from pyscipopt import Model
+
+
+pattern = re.compile(r"(?:Button A|Button B|Prize): X[\+=]([0-9]*), Y[\+=]([0-9]*)")
+
+
+def splitline(line):
+    m = pattern.match(line)
+    return tuple([int(m.group(1)), int(m.group(2))])
+
+
+total_tokens = 0
+for claw_machine in INPUT.split("\n\n"):
+    claw_machine = claw_machine.split("\n")
+
+    Ax, Ay = splitline(claw_machine[0])
+    Bx, By = splitline(claw_machine[1])
+    Tx, Ty = splitline(claw_machine[2])
+
+    Tx += 10000000000000
+    Ty += 10000000000000
+
+    model = Model("Claw Machine")
+    a = model.addVar("a", vtype="INTEGER")
+    b = model.addVar("b", vtype="INTEGER")
+    model.setObjective(3 * a + 1 * b)
+    model.addCons(a * Ax + b * Bx == Tx)
+    model.addCons(a * Ay + b * By == Ty)
+    model.optimize()
+    sol = model.getBestSol()
+    if model.getStatus() != "infeasible":
+        sol_a = int(sol[a])
+        sol_b = int(sol[b])
+
+        # due to precision issues (?) not all solutions fulfill the constraint exactly
+        # so we chack here again.
+        if sol_a * Ax + sol_b * Bx == Tx and sol_a * Ay + sol_b * By == Ty:
+            tokens = 3 * sol_a + 1 * sol_b
+            total_tokens += tokens
+
+print(total_tokens)
